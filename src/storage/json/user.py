4d7808a032b3b3e8cd85c3473eec87a616e2621e
Caio Oliveira <caio@caiooliveira.eti.br>
2018-06-09 14:16:14 -0300

    Change user parser

--- src/storage/json/user.py
+++ src/storage/json/user.py
@@ -34,45 +34,35 @@ class JsonFileUserStorage(UserStorage):
 
     def dumpToFile(self):
         users_file = open('users.json', 'w')
-        json_data = json.dumps(self.users, cls=JsonObjectEncoder, indent=2, sort_keys=False)
+        json_data = json.dumps(self.users, cls=JsonUser, indent=2, sort_keys=False)
         users_file.write(json_data)
         users_file.close()
 
     def loadFromFile(self):
         try:
             users_file = open('users.json', 'r')
-            self.users = list(map(lambda user: self.userJsonParser(user), json.load(users_file)))
+            self.users = list(map(lambda user: JsonUser.userDecode(user), json.load(users_file)))
             users_file.close()
         except FileNotFoundError:
             self.dumpToFile()
 
+
+class JsonUser(json.JSONEncoder):
+    def default(self, user: User):
+        return self.userEncode(user)
+
     @staticmethod
-    def userJsonParser(user):
+    def userEncode(user: User):
+        return {
+            "name": user.name,
+            "nickname": user.nickname,
+            "responsibility": user.responsibility,
+            "profile": user.profile.name,
+            "date_last_access": user.date_last_access,
+            "time_last_access": user.time_last_access
+        }
+
+    @staticmethod
+    def userDecode(user):
         user['profile'] = UserProfile[user['profile']]
         return User(**user)
-
-
-class JsonObjectEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if hasattr(obj, "to_json"):
-            return self.default(obj.to_json())
-        elif hasattr(obj, "__dict__"):
-            d = {}
-            for key, value in inspect.getmembers(obj):
-                if not key.startswith("__") \
-                        and not inspect.isabstract(value) \
-                        and not inspect.isbuiltin(value) \
-                        and not inspect.isfunction(value) \
-                        and not inspect.isgenerator(value) \
-                        and not inspect.isgeneratorfunction(value) \
-                        and not inspect.ismethod(value) \
-                        and not inspect.ismethoddescriptor(value) \
-                        and not inspect.isroutine(value):
-
-                    if isinstance(value, Enum):
-                        d[key] = value.name
-                    else:
-                        d[key] = value
-
-            return self.default(d)
-        return obj
